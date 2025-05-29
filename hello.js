71eec313eb2a9aa6de14a256563f7a849ee98ee2
Naim <naimur2210026@gmail.com>
2025-05-29 17:00:02 +0600

    2 + 2 = 5 was added to hello.js

--- hello.js
+++ hello.js
@@ -2,3 +2,4 @@ console.log("hello git!");
 console.log("Hello github");
 console.log("This should be a good commit");
 console.log("This is a bad commit");
+console.log("Here 2 + 2 = 5"); //it is a bad code & as well as the commit
