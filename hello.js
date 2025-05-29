0f4e97f5bb7e584a1ea134e3384170d433242a3a
Naim <naimur2210026@gmail.com>
2025-05-29 17:29:31 +0600

    deleted last 2 lines in hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,4 @@
 console.log("hello git!");
 console.log("Hello github");
 console.log("This should be a good commit");
-console.log("This is a bad commit");
-console.log("Here 2 + 2 = 5"); //it is a bad code & as well as the commit
+
